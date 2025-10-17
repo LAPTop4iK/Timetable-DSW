ad929f5d9dcd48ef8b71e0e3447c60806ec3b069
Mikita Laptsionak <nikita.laptenok@gmail.com>
2025-10-17 02:57:58 +0200

    Initial Commit

---
+++ Timetable DSW/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Timetable DSW
+//
+//  Created by Mikita Laptsionak on 17/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
