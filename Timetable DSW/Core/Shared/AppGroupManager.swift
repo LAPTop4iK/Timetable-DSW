68557baf8ea6839142b48d65783d42e5a60a9f0c
Mikita Laptsionak <nikita.laptenok@gmail.com>
2025-10-24 05:59:12 +0200

    Fix widget (wip), group selection, events filter

--- Timetable DSW/Core/Shared/AppGroupManager.swift
+++ Timetable DSW/Core/Shared/AppGroupManager.swift
@@ -9,7 +9,7 @@ import Foundation
 
 struct AppGroupManager {
     // ВАЖНО: Замените на свой App Group ID из Xcode
-    static let appGroupIdentifier = "org.laptenok.Timetable-DSW.rl"
+    static let appGroupIdentifier = "group.org.laptenok.Timetable-DSW.rl"
 
     static var sharedDefaults: UserDefaults? {
         UserDefaults(suiteName: appGroupIdentifier)
