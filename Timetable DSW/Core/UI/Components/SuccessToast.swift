bf8d008a32d34e6183cf477f5a38bb7b97e89099
Mikita Laptsionak <nikita.laptenok@gmail.com>
2025-10-27 20:07:32 +0100

    Fix build

--- Timetable DSW/Core/UI/Components/SuccessToast.swift
+++ Timetable DSW/Core/UI/Components/SuccessToast.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Combine
 
 struct SuccessToast: View {
     let message: String
--- Timetable DSW/Features/Settings/Views/SettingsView.swift
+++ Timetable DSW/Features/Settings/Views/SettingsView.swift
@@ -6,6 +6,7 @@
 import SwiftUI
 import Combine
 import Foundation
+import StoreKit
 
 struct SettingsView: View {
     struct Configuration: ComponentConfiguration {
